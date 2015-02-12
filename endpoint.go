d435870d454a32cc5ba74d06221680714144235e
Jan Berktold <janberktold@aol.com>
2015-02-13 00:37:58 +0100

    Create Message type

--- endpoint.go
+++ endpoint.go
@@ -1,10 +1,19 @@
 package rock7
 
 import (
+	"github.com/kellydunn/golang-geo"
 	"net/http"
+	"time"
 )
 
 type Message struct {
+	IMEI         string
+	MOMSN        int
+	TransmitTime time.Time
+	IridumPos    geo.Point
+	IridiumCep   int
+	HexData      string
+	Data         string
 }
 
 type Endpoint struct {
@@ -12,7 +21,15 @@ type Endpoint struct {
 }
 
 func NewEndpoint() *Endpoint {
-	return &Endpoint{}
+	return &Endpoint{
+		make(chan Message),
+	}
+}
+
+// Returns a channel contained all recieved messages.
+// Getter method is required due to access locking.
+func (end *Endpoint) GetChannel() <-chan Message {
+	return end.channel
 }
 
 func (end *Endpoint) ServeHTTP(http.ResponseWriter, *http.Request) {
