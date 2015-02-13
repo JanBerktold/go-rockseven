21294d036e2d95bab099eb1389cbbfdd0b740429
Jan Berktold <janberktold@aol.com>
2015-02-13 13:38:22 +0100

    Write ServeHttp method and test

--- endpoint.go
+++ endpoint.go
@@ -1,8 +1,10 @@
 package rock7
 
 import (
+	"encoding/hex"
 	"github.com/kellydunn/golang-geo"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -10,7 +12,7 @@ type Message struct {
 	IMEI         string
 	MOMSN        int
 	TransmitTime time.Time
-	IridumPos    geo.Point
+	IridumPos    *geo.Point
 	IridiumCep   int
 	HexData      string
 	Data         string
@@ -32,11 +34,51 @@ func (end *Endpoint) GetChannel() <-chan Message {
 	return end.channel
 }
 
+func readInt(n string) int {
+	num, mErr := strconv.Atoi(n)
+
+	if mErr != nil {
+		panic("Invalid request, panic'ing to cancel request")
+	}
+	return num
+}
+
+func readFloat64(n string) float64 {
+	num, mErr := strconv.ParseFloat(n, 64)
+
+	if mErr != nil {
+		panic("Invalid request, panic'ing to cancel request")
+	}
+	return num
+}
+
 func (end *Endpoint) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 
 	if req.Method != "POST" {
 		writer.WriteHeader(http.StatusBadRequest)
 		return
+	}
+
+	data := req.FormValue("data")
+	by, err := hex.DecodeString(data)
+
+	// TODO: Read time
+	if err == nil {
+		end.channel <- Message{
+			req.FormValue("imei"),
+			readInt(req.FormValue("momsn")),
+			time.Now(),
+			geo.NewPoint(
+				readFloat64(req.FormValue("iridium_latitude")),
+				readFloat64(req.FormValue("iridium_longitude")),
+			),
+			readInt(req.FormValue("iridium_cep")),
+			data,
+			string(by),
+		}
+		writer.WriteHeader(http.StatusOK)
+	} else {
+		writer.WriteHeader(http.StatusBadRequest)
 	}
 
 }
--- endpoint_test.go
+++ endpoint_test.go
@@ -1,10 +1,61 @@
 package rock7
 
 import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"github.com/kellydunn/golang-geo"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
+	"time"
 )
+
+func fakeRequest(handler http.Handler, msg string) (code int, returnBody string) {
+	req := constructRequest(msg)
+	w := httptest.NewRecorder()
+	handler.ServeHTTP(w, req)
+
+	return w.Code, w.Body.String()
+}
+
+func constructRequest(msg string) *http.Request {
+	param := url.Values{}
+	param.Add("imei", "123456789")
+	param.Add("momsn", "12345")
+	param.Add("transmit_time", time.Now().Format("12-10-10 10:41:50"))
+	param.Add("iridium_latitude", "54.123")
+	param.Add("iridium_longitude", "23.987")
+	param.Add("iridium_cep", "2")
+	param.Add("data", hex.EncodeToString([]byte(msg)))
+	req, _ := http.NewRequest("POST", "http://localhost/recieve", bytes.NewBufferString(param.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	return req
+}
+
+func compareMessage(msg Message, body string) bool {
+	return msg.Data == body && msg.MOMSN == 12345 && msg.IMEI == "123456789" && msg.IridiumCep == 2 && *msg.IridumPos == *geo.NewPoint(54.123, 23.987)
+}
 
 func TestInterface(t *testing.T) {
 	http.Handle("/recieve", NewEndpoint())
+}
+
+func TestSimpleMessage(t *testing.T) {
+	endpoint := NewEndpoint()
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			fakeRequest(endpoint, fmt.Sprintf("Request %v", i))
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		message := <-endpoint.GetChannel()
+		if !compareMessage(message, fmt.Sprintf("Request %v", i)) {
+			t.Fatalf("Failed")
+		}
+	}
+
 }
